0572cad808f829a8a0695e18c7a2d2085ec7ef68
Ahmed Ali <ahmed.o.ali430@gmail.com>
2025-05-13 15:28:10 +0300

    Add hive for app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:untitled/Screen/Note_View.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox("note box");
   runApp(const NoteApp());
 }
 
@@ -12,10 +15,10 @@ class NoteApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        brightness:Brightness.dark,
+        brightness: Brightness.dark,
         fontFamily: "Edu Australia VIC WA NT Hand",
       ),
-     home: NoteView()
-     );
+      home: NoteView(),
+    );
   }
 }
